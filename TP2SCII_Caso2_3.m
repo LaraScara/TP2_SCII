4df71138ec3dfef5e52b407b43c17755b387f8fd
LaraScara <103203944+LaraScara@users.noreply.github.com>
2023-07-19 23:30:27 -0300

    Update TP2SCII_Caso2_3.m

--- TP2SCII_Caso2_3.m
+++ TP2SCII_Caso2_3.m
@@ -14,14 +14,14 @@ tiempo = round(tF/t_etapa);
 % Matrices x = [delta ; delta_p ; phi ; phi_p]
 A = [0 1 0 0;0 -F/M -m*g/M 0; 0 0 0 1; 0 F/(l*M) g*(m+M)/(l*M) 0];
 B = [0; 1/M; 0; -1/(l*M)];
-C = [1 0 0 0];
+C = [1 0 0 0; 0 0 1 0];
 D = 0;
 
 % Controlabilidad y observabilidad
 Co = ctrb(A, B);
-rank(Co); % = 4 por ende es controlable
+rank(Co) % = 4 por ende es controlable
 Ob = obsv(A,C);
-rank(Ob); % = 4 por ende es observable
+rank(Ob) % = 4 por ende es observable
 
 % LQR
 Q = diag([1 1 1000 10000]);
@@ -29,7 +29,7 @@ R = 10;
 K = lqr(A,B,Q,R);
 
 % Ganancia de prealimentación
-G = -inv(C*inv(A-B*K)*B);
+G = -inv(C(1,:)*inv(A-B*K)*B);
 
 % Referencia
 ref = 0*t - 10;
@@ -67,10 +67,10 @@ legend('u(t)');
 grid on;
 
 figure
-plot(t,delta,'r');
+plot(t,ref,'b');
 grid on;
 hold on;
-plot(t,ref);
+plot(t,delta,'r');
 title('Desplazamiento del carro');
 xlabel('Tiempo [s]');
 ylabel('Posición [m]');
@@ -83,6 +83,5 @@ xlabel('Tiempo [s]');
 ylabel('Ángulo [rad]');
 legend('θ(t)');
 grid on;
-
 
 disp("Terminado")
